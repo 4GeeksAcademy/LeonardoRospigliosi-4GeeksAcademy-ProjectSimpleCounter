cce1693d46ab57672514eed9b9a42b5dc932b622
Leonardo Rospigliosi <70114762+LeonardoRospigliosi@users.noreply.github.com>
2025-02-22 05:57:25 +0000

    reloj leo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,51 +1,27 @@
-import React from "react";
-import MiComponente from './MiComponente.jsx';
-import Card from './Card.jsx';
+import React, { useState, useEffect } from "react";
 import SecondsCounter from './SecondsCounter.jsx';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-
-
-
-//create your first component
 const Home = () => {
-	return (
-		
+  const [segundo, setSegundo] = useState(0); // Segundo Inicial
 
-		<div className="text-center">
-			{/* <SecondsCounter seconds={3434} /> */}
-			
-			
-			{/* <SecondsCounter  num1={valor1} num2={valor2} num3={valor3} num4={valor4} num5={valor5} num6={valor6}/> */}
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSegundo(contador => contador + 1); // Incremento segundo +1
+    }, 1000);
+  
+    return () => {
+      clearInterval(timer); // Limpia el intervalo cuando el componente se desmonta
+    };
+  }, []); // Se ejecuta solo una vez cuando el componente se monta
 
 
+  return (
+    <div className="text-center">
+      <SecondsCounter pSegundo={segundo} />
 			{/* <Card imageUrl="https://picsum.photos/id/124/350/150" title="A nice image" />
 			<MiComponente nombre="Abreu" edad={50} esProfesor={true} /> */}
-		</div>
-	);
+    </div>
+  );
 };
-
-
-let contador=0;
-setInterval(() => {
-    const valor1 = Math.floor(contador/1);
-    const valor2 = Math.floor(contador/10);
-    const valor3 = Math.floor(contador/100);
-    const valor4 = Math.floor(contador/1000);
-    const valor5 = Math.floor(contador/10000);
-    const valor6 = Math.floor(contador/100000);
-    contador = contador + 1
-
-	Home.render(
-        <SecondsCounter digitOne={valor1} digitTwo={valor2} digitThree={valor3} digitFour={valor4} />
-    );
-
-  }, 1000);
-
-
-
-
 
 export default Home;
--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,40 +1,28 @@
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
+export default function SecondsCounter({ pSegundo }) {
+  // Separar los segundos en digitos individuales
+  const valor1 = Math.floor(pSegundo / 1) % 10;
+  const valor2 = Math.floor(pSegundo / 10) % 10;
+  const valor3 = Math.floor(pSegundo / 100) % 10;
+  const valor4 = Math.floor(pSegundo / 1000) % 10;
+  const valor5 = Math.floor(pSegundo / 10000) % 10;
+  const valor6 = Math.floor(pSegundo / 100000) % 10;
 
-
-
-
-export default function SecondsCounter(props){
-    return (
-        <div className="bigCounter mt-5 d-inline-flex fs-2">
-            <div className="div7 m-2 "><i className="fa-regular fa-clock"></i></div>
-            <div className="div6 m-2 p-2">{props.num6 %10}</div>
-            <div className="div5 m-2 p-2">{props.num5 %10}</div>
-            <div className="div4 m-2 p-2">{props.num4 %10}</div>
-            <div className="div3 m-2 p-2">{props.num3 %10}</div>
-            <div className="div2 m-2 p-2">{props.num2 %10}</div>
-            <div className="div1 m-2 p-2">{props.num1 %10}</div>
-        </div>
-    );
+  return (
+    <div className="bigCounter mt-5 d-inline-flex fs-2">
+      <div className="div7 m-2"><i className="fa-regular fa-clock"></i></div>
+      <div className="div6 m-2 p-2">{valor6}</div>
+      <div className="div5 m-2 p-2">{valor5}</div>
+      <div className="div4 m-2 p-2">{valor4}</div>
+      <div className="div3 m-2 p-2">{valor3}</div>
+      <div className="div2 m-2 p-2">{valor2}</div>
+      <div className="div1 m-2 p-2">{valor1}</div>
+    </div>
+  );
 }
 
-SecondsCounter.propTypes={
-    num1:PropTypes.number,
-    num2:PropTypes.number,
-    num3:PropTypes.number,
-    num4:PropTypes.number,
-    num5:PropTypes.number,
-    num6:PropTypes.number
+SecondsCounter.propTypes = {
+    pSegundo: PropTypes.number.isRequired, // Asegura que parametro 'segundos' sea pasado como numero
 };
-
-let contador=0;
-setInterval(() => {
-    const valor1 = Math.floor(contador/1);
-    const valor2 = Math.floor(contador/10);
-    const valor3 = Math.floor(contador/100);
-    const valor4 = Math.floor(contador/1000);
-    const valor5 = Math.floor(contador/10000);
-    const valor6 = Math.floor(contador/100000);
-    contador = contador + 1
-  }, 1000);
-
